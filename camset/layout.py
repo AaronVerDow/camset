c35c6bfa1501348134cd0b5a9376074de86b91ec
Aaron VerDow <aaron@verdow.com>
2025-05-30 17:50:15 -0500

    change gap when scaling window larger

--- camset/layout.py
+++ camset/layout.py
@@ -126,11 +126,11 @@ class Layout:
         self.win.main_box.pack_start(self.win.warningcontainer, False, False, 0)
         self.win.main_box.pack_start(device_row, False, False, 0)
         
-        # Slider controls first (need most space)
+        # Slider controls first (don't expand - just take what they need)
         int_frame = Gtk.Frame(label="Slider Controls")
         int_frame.set_margin_top(5)
         int_frame.add(self.win.int_control_box)
-        self.win.main_box.pack_start(int_frame, True, True, 0)  # Allow sliders to expand
+        self.win.main_box.pack_start(int_frame, False, False, 0)  # Changed from True, True to False, False
         
         # Dropdown controls second
         menu_frame = Gtk.Frame(label="Dropdown Controls")
@@ -138,11 +138,11 @@ class Layout:
         menu_frame.add(self.win.menu_control_box)
         self.win.main_box.pack_start(menu_frame, False, False, 0)
         
-        # Switch controls last
+        # Switch controls last (allow this section to expand and fill extra space)
         bool_frame = Gtk.Frame(label="Switch Controls")
         bool_frame.set_margin_top(5)
         bool_frame.add(self.win.bool_control_box)
-        self.win.main_box.pack_start(bool_frame, False, False, 0)
+        self.win.main_box.pack_start(bool_frame, True, True, 0)  # Changed from False, False to True, True
 
     def setup_resolution(self):
         # Create horizontal box for resolution label and control
