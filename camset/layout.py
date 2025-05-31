41d7fc36977884374a4a79d647dc693ae89b4b30
Aaron VerDow <aaron@verdow.com>
2025-05-31 09:28:06 -0500

    clean up variables

--- camset/layout.py
+++ camset/layout.py
@@ -3,6 +3,12 @@ import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
+LABEL_WIDTH = 180
+WIDGET_HEIGHT = 30
+MARGIN = 10
+SPACING = 5
+MIN_WIDTH = 100  # Not sure what this does
+
 
 class Layout:
     def __init__(self, win, dialogs):
@@ -17,40 +23,40 @@ class Layout:
         self.win.add(self.win.layout)
 
         # Use a Box instead of Grid for better flexibility
-        self.win.main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=5)
-        self.win.main_box.set_margin_start(10)
-        self.win.main_box.set_margin_end(10)
-        self.win.main_box.set_margin_top(10)
-        self.win.main_box.set_margin_bottom(10)
+        self.win.main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=SPACING)
+        self.win.main_box.set_margin_start(MARGIN)
+        self.win.main_box.set_margin_end(MARGIN)
+        self.win.main_box.set_margin_top(MARGIN)
+        self.win.main_box.set_margin_bottom(MARGIN)
         self.win.layout.add(self.win.main_box)
 
     def setup_boxes(self):
         # Create responsive container that can switch between horizontal and vertical layout
-        self.win.control_sections_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+        self.win.control_sections_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=MARGIN)
         self.win.control_sections_box.set_hexpand(True)
         self.win.control_sections_box.set_vexpand(True)
 
         # Menu controls section - just the control box, labels are integrated
-        self.win.menu_section = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=3)
-        self.win.menu_control_box = Gtk.Box(spacing=3, orientation=Gtk.Orientation.VERTICAL)
+        self.win.menu_section = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=SPACING)
+        self.win.menu_control_box = Gtk.Box(spacing=SPACING, orientation=Gtk.Orientation.VERTICAL)
 
         # Bool controls section - just the control box, labels are integrated
-        self.win.bool_section = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=3)
-        self.win.bool_control_box = Gtk.Box(spacing=3, orientation=Gtk.Orientation.VERTICAL)
+        self.win.bool_section = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=SPACING)
+        self.win.bool_control_box = Gtk.Box(spacing=SPACING, orientation=Gtk.Orientation.VERTICAL)
 
         # Int controls section - just the control box, labels are integrated
-        self.win.int_section = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=3)
-        self.win.int_control_box = Gtk.Box(spacing=3, orientation=Gtk.Orientation.VERTICAL)
+        self.win.int_section = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=SPACING)
+        self.win.int_control_box = Gtk.Box(spacing=SPACING, orientation=Gtk.Orientation.VERTICAL)
 
         # Device selection
-        self.win.devicelabelbox = Gtk.Box(spacing=5, orientation=Gtk.Orientation.VERTICAL)
-        self.win.devicecontrolbox = Gtk.Box(spacing=5, orientation=Gtk.Orientation.VERTICAL)
+        self.win.devicelabelbox = Gtk.Box(spacing=SPACING, orientation=Gtk.Orientation.VERTICAL)
+        self.win.devicecontrolbox = Gtk.Box(spacing=SPACING, orientation=Gtk.Orientation.VERTICAL)
 
     def setup_device_selection_box(self):
         self.win.device_selection = Gtk.ComboBox()
         self.win.device_selection.set_hexpand(True)
         # Reduce the height of the combobox
-        self.win.device_selection.set_size_request(-1, 30)
+        self.win.device_selection.set_size_request(-1, WIDGET_HEIGHT)
         self.win.label = Gtk.Label(label="Device")
         self.win.label.set_halign(Gtk.Align.START)
         self.win.devicecontrolbox.pack_start(self.win.device_selection, True, True, 0)
@@ -63,15 +69,15 @@ class Layout:
 
     def setup_buttons(self):
         # Create a horizontal box for buttons
-        self.win.button_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
+        self.win.button_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=MARGIN)
         self.win.button_box.set_halign(Gtk.Align.CENTER)
 
         self.win.btn_defaults = Gtk.Button(label="Load defaults")
-        self.win.btn_defaults.set_size_request(-1, 30)  # Reduce button height
+        self.win.btn_defaults.set_size_request(-1, WIDGET_HEIGHT)  # Reduce button height
         self.win.btn_defaults.connect("clicked", self.win.on_btn_defaults_clicked)
 
         self.win.btn_showcam = Gtk.ToggleButton(label="Show camera feed")
-        self.win.btn_showcam.set_size_request(-1, 30)  # Reduce button height
+        self.win.btn_showcam.set_size_request(-1, WIDGET_HEIGHT)  # Reduce button height
         self.win.btn_showcam.connect("toggled", self.win.on_btn_showcam_toggled)
 
         self.win.button_box.pack_start(self.win.btn_defaults, False, False, 0)
@@ -85,10 +91,10 @@ class Layout:
         self.win.warning.set_reveal_child(False)
         self.win.warningmessage = Gtk.TextView()
         self.win.warningmessage.set_editable(False)
-        self.win.warningmessage.set_left_margin(10)
-        self.win.warningmessage.set_right_margin(10)
-        self.win.warningmessage.set_top_margin(5)
-        self.win.warningmessage.set_bottom_margin(5)
+        self.win.warningmessage.set_left_margin(MARGIN)
+        self.win.warningmessage.set_right_margin(MARGIN)
+        self.win.warningmessage.set_top_margin(MARGIN / 2)
+        self.win.warningmessage.set_bottom_margin(MARGIN / 2)
         self.win.warningmessage.props.halign = Gtk.Align.CENTER
         self.win.warning.add(self.win.warningmessage)
         self.win.warningcontainer.add(self.win.warning)
@@ -117,7 +123,7 @@ class Layout:
 
     def setup_grid(self):
         # Create device selection row
-        device_row = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
+        device_row = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=MARGIN)
         device_row.pack_start(self.win.devicelabelbox, False, False, 0)
         device_row.pack_start(self.win.devicecontrolbox, True, True, 0)
         device_row.pack_start(self.win.button_box, False, False, 0)
@@ -129,36 +135,36 @@ class Layout:
 
         # Slider controls first (don't expand - just take what they need)
         int_container = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-        int_container.set_margin_top(5)
+        int_container.set_margin_top(MARGIN / 2)
         int_container.add(self.win.int_control_box)
         self.win.main_box.pack_start(int_container, False, False, 0)
 
         # Dropdown controls second
         menu_container = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-        menu_container.set_margin_top(5)
+        menu_container.set_margin_top(MARGIN / 2)
         menu_container.add(self.win.menu_control_box)
         self.win.main_box.pack_start(menu_container, False, False, 0)
 
         # Switch controls last (allow this section to expand and fill extra space)
         bool_container = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-        bool_container.set_margin_top(5)
+        bool_container.set_margin_top(MARGIN / 2)
         bool_container.add(self.win.bool_control_box)
         self.win.main_box.pack_start(bool_container, True, True, 0)
 
     def setup_resolution(self):
         # Create horizontal box for resolution label and control
-        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
+        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=MARGIN)
 
         label = Gtk.Label()
         label.set_text("Video Resolution")
         label.set_halign(Gtk.Align.END)
         label.set_valign(Gtk.Align.CENTER)
-        label.set_size_request(180, 25)  # Increased to 180 for more uniform appearance
+        label.set_size_request(LABEL_WIDTH, WIDGET_HEIGHT)
         hbox.pack_start(label, False, False, 0)
 
         self.win.resolution_selection = Gtk.ComboBox()
         self.win.resolution_selection.set_hexpand(True)
-        self.win.resolution_selection.set_size_request(300, 30)  # Set consistent minimum width
+        self.win.resolution_selection.set_size_request(MIN_WIDTH, WIDGET_HEIGHT)  # Set consistent minimum width
         cell = Gtk.CellRendererText()
         self.win.resolution_selection.pack_start(cell, True)
         self.win.resolution_selection.add_attribute(cell, "text", 0)
@@ -171,7 +177,7 @@ class Layout:
     def add_menu_item(self, setting, action):
         self.win.ctrl_combobox = Gtk.ComboBox()
         self.win.ctrl_combobox.set_hexpand(True)
-        self.win.ctrl_combobox.set_size_request(-1, 30)  # Reduce height
+        self.win.ctrl_combobox.set_size_request(-1, WIDGET_HEIGHT)  # Reduce height
         self.win.ctrl_store = Gtk.ListStore(str)
         cell = Gtk.CellRendererText()
         self.win.ctrl_combobox.pack_start(cell, True)
@@ -192,7 +198,7 @@ class Layout:
         scale.set_digits(0)
         scale.set_value_pos(Gtk.PositionType.RIGHT)
         scale.set_hexpand(True)
-        scale.set_size_request(200, 30)  # Set minimum width and reduce height
+        scale.set_size_request(200, WIDGET_HEIGHT)  # Set minimum width and reduce height
         scale.connect("value-changed", action, setting)
         self.win.int_control_box.pack_start(scale, True, True, 0)
 
@@ -209,18 +215,18 @@ class Layout:
 
     def add_menu_item_with_label(self, setting, action, label):
         # Create a horizontal box for this label-control pair
-        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
+        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=MARGIN)
 
         # Add the label
         label.set_halign(Gtk.Align.END)
         label.set_valign(Gtk.Align.CENTER)
-        label.set_size_request(180, 25)  # Increased to 180 for more uniform appearance
+        label.set_size_request(LABEL_WIDTH, WIDGET_HEIGHT)
         hbox.pack_start(label, False, False, 0)
 
         # Create the combobox
         self.win.ctrl_combobox = Gtk.ComboBox()
         self.win.ctrl_combobox.set_hexpand(True)
-        self.win.ctrl_combobox.set_size_request(300, 30)  # Set consistent minimum width
+        self.win.ctrl_combobox.set_size_request(MIN_WIDTH, WIDGET_HEIGHT)  # Set consistent minimum width
         self.win.ctrl_store = Gtk.ListStore(str)
         cell = Gtk.CellRendererText()
         self.win.ctrl_combobox.pack_start(cell, True)
@@ -234,12 +240,12 @@ class Layout:
 
     def add_int_item_with_label(self, line, setting, value, action, label):
         # Create a horizontal box for this label-control pair
-        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
+        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=MARGIN)
 
         # Add the label
         label.set_halign(Gtk.Align.END)
         label.set_valign(Gtk.Align.CENTER)
-        label.set_size_request(180, 25)  # Increased to 180 for more uniform appearance
+        label.set_size_request(LABEL_WIDTH, WIDGET_HEIGHT)
         hbox.pack_start(label, False, False, 0)
 
         # Create the slider
@@ -254,7 +260,7 @@ class Layout:
         scale.set_digits(0)
         scale.set_value_pos(Gtk.PositionType.RIGHT)
         scale.set_hexpand(True)
-        scale.set_size_request(300, 30)  # Set consistent minimum width to match dropdowns
+        scale.set_size_request(MIN_WIDTH, WIDGET_HEIGHT)  # Set consistent minimum width to match dropdowns
         scale.connect("value-changed", action, setting)
         hbox.pack_start(scale, True, True, 0)
 
@@ -263,12 +269,12 @@ class Layout:
 
     def add_bool_item_with_label(self, setting, value, action, label):
         # Create a horizontal box for this label-control pair
-        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
+        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=MARGIN)
 
         # Add the label
         label.set_halign(Gtk.Align.END)
         label.set_valign(Gtk.Align.CENTER)
-        label.set_size_request(180, 25)  # Increased to 180 for more uniform appearance
+        label.set_size_request(LABEL_WIDTH, WIDGET_HEIGHT)
         hbox.pack_start(label, False, False, 0)
 
         # Create the switch
@@ -284,4 +290,3 @@ class Layout:
 
         # Add the horizontal box to the bool control box
         self.win.bool_control_box.pack_start(hbox, False, False, 0)
-
